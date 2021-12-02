87be3dbc1f51040f0b4080d931a323f8ff91343a
Imtiaz Uddin <iuddin@usc.edu>
2021-12-01 21:46:59 -0800

    server1

---
+++ project_files/tcp_client.py
@@ -0,0 +1,30 @@
+"""
+Server receiver buffer is char[256]
+If correct, the server will send a message back to you saying "I got your message"
+Write your socket client code here in python
+Establish a socket connection -> send a short message -> get a message back -> ternimate
+use python "input->" function, enter a line of a few letters, such as "abcd"
+"""
+
+# Github repo link: https://github.com/usc-ee250-fall2021/lab03-lab3-imtiaz/tree/lab03/ee250/lab03
+# Team Member Name: Imtiaz Uddin
+
+import socket
+
+def main():
+    
+    # TODO: Create a socket and connect it to the server at the designated IP and port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("40.118.164.114", 8080))
+    # For testing on local machine: "127.0.0.1", 10002
+    
+    # TODO: Get user input and send it to the server using your TCP socket
+    text = input("Enter user input: ").encode()
+    s.send(text)
+    
+    # TODO: Receive a response from the server and close the TCP connection
+    print(s.recv(1024).decode())
+    s.close()
+
+if __name__ == '__main__':
+    main()
