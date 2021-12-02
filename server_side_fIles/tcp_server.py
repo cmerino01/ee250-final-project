468af974fa7c69413c26e0d0a12d73fff56467e4
Christopher Merino Brito <merinobr@usc.edu>
2021-12-02 15:10:55 -0800

    server files

--- server_side_fIles/tcp_server.py
+++ server_side_fIles/tcp_server.py
@@ -1,14 +1,18 @@
 import socket
-import json
+import pickle
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("168.62.194.224", 8080))
+s.bind(("0.0.0.0", 8080))
 s.listen()
 conn, addr = s.accept()
 with conn:
     print('Connected by', addr)
     while True:
         data = conn.recv(1024)
+        print("Received size:" + str(sys.getsizeof(data)))
+        crypto_data = pickle.loads(data)
+        #crypto_data  = json.loads(crypto_data)
         if not data:
-            break
-        conn.sendall(data)
+            continue
+        conn.sendall(("checkpoint").encode())
